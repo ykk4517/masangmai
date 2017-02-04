d3d29c83e635c42202fe464c10bcd59b878d3e74
xsdhy <402837074@qq.com>
2017-02-04 12:51:40 +0800

    马上买

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 	$config = array(
 		'dbconfig' => array(
-			'dbhost' => 'localhost', 'dbuser'=>'root', 'dbpsw' => 'xsdhy123456' , 'dbname' => 'shop', 'dbcharset' => 'utf8')
+			'dbhost' => 'localhost', 'dbuser'=>'root', 'dbpsw' => 'root' , 'dbname' => 'shop', 'dbcharset' => 'utf8')
 	);
 ?>
